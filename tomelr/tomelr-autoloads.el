ee85499a4fe9dcd437209aa0ab9d134973b0033c
threddast <giorgiserik@gmail.com>
2022-09-02 11:28:46 +0200

    update

--- tomelr/tomelr-autoloads.el
+++
@@ -1,20 +0,0 @@
-;;; tomelr-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "tomelr" "tomelr.el" (0 0 0 0))
-;;; Generated autoloads from tomelr.el
-
-(register-definition-prefixes "tomelr" '("tomelr-"))
-
-;;;***
-
-(provide 'tomelr-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; tomelr-autoloads.el ends here
